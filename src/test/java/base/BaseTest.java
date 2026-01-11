11b657a2b3a5787198b1c0a579dad3b29d59dcfb
Ashwini Raj <37695456+ashwinirajm@users.noreply.github.com>
2026-01-11 17:01:08 +0530

    Update BaseTest.java

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,1 +1,22 @@
+package base;
+
+import core.driver.DriverFactory;
+import core.driver.DriverManager;
+import io.appium.java_client.AppiumDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+
+    @BeforeMethod(alwaysRun = true)
+    public void setUp() {
+        AppiumDriver driver = DriverFactory.createDriver();
+        DriverManager.setDriver(driver);
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        DriverManager.quitDriver();
+    }
+}
 
