6f8c9684b50c3e41ee7745f8da26540b59e70793
Ashwini Raj <37695456+ashwinirajm@users.noreply.github.com>
2026-01-13 00:40:39 +0530

    Update LoginPage.java

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,1 +1,18 @@
+package pages.login;
 
+import global.PageUtils;
+import io.appium.java_client.pagefactory.*;
+import java.util.Objects;
+import org.openqa.selenium.WebElement;
+
+
+public class NewLoginPage extends PageUtils {
+
+private void inputMobileNumberNewApp(String mobileNum) {
+    waitUntilVisible(mobileInput);
+    mobileInput.sendKeys(mobileNum);
+    waitUntilClickable(creditFetchCheckBox);
+    creditFetchCheckBox.click();
+    waitUntilClickable(proceedBtn);
+    proceedBtn.click();
+}
