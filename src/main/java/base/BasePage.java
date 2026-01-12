0820ddb97561fc077099a757e2596b220a5ec27e
Ashwini Raj <37695456+ashwinirajm@users.noreply.github.com>
2026-01-13 01:01:56 +0530

    Update BasePage.java

--- src/main/java/base/BasePage.java
+++ src/main/java/base/BasePage.java
@@ -1,1 +1,34 @@
+package base;
 
+import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public abstract class BasePage {
+
+    protected AppiumDriver driver;
+    private final WebDriverWait wait;
+
+    protected BasePage(AppiumDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    /* -------------------- WAIT HELPERS -------------------- */
+
+    protected void waitUntilVisible(WebElement element) {
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    protected void waitUntilClickable(WebElement element) {
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    protected void waitUntilPresent(By locator) {
+        wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+    }
+}
