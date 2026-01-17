670e1a15a2dc7ccd2bde951a88d9fb4e501c18a6
Ashwini Raj <37695456+ashwinirajm@users.noreply.github.com>
2026-01-18 02:55:40 +0530

    Update LoginPage.java

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -9,45 +9,35 @@ import utils.LoggerUtils;
 
 public class LoginPage extends BasePage {
 
-    // -------------------- ELEMENT LOCATORS --------------------
-    private final By mobileInput = MobileBy.AccessibilityId("mobileInput");
-    private final By creditFetchCheckBox = MobileBy.AccessibilityId("creditFetchCheckBox");
-    private final By proceedBtn = MobileBy.AccessibilityId("proceedBtn");
-
-    @AndroidFindBy(className = "android.widget.EditText")
-    @iOSXCUITFindBy(xpath = "//XCUIElementTypeTextField")
+    @AndroidFindBy(accessibility = "mobileInput")
+    @iOSXCUITFindBy(accessibility = "mobileInput")
     private WebElement mobileInput;
 
+    @AndroidFindBy(accessibility = "creditFetchCheckBox")
+    @iOSXCUITFindBy(accessibility = "creditFetchCheckBox")
+    private WebElement creditFetchCheckBox;
+
+    @AndroidFindBy(accessibility = "proceedCTA")
+    @iOSXCUITFindBy(accessibility = "proceedCTA")
+    private WebElement proceedCTA;
     
-    // -------------------- CONSTRUCTOR --------------------
+
     public LoginPage(AppiumDriver driver) {
         super(driver);
     }
 
-    // -------------------- BUSINESS METHODS --------------------
-
-    /**
-     * Enters the mobile number and clicks through login flow
-     * @param mobileNum Mobile number to login with
-     */
+    
     public void inputMobileNumberNewApp(String mobileNum) {
-        // Wait until mobile input is visible and enter the number
         waitUntilVisible(mobileInput));
         driver.findElement(mobileInput).sendKeys(mobileNum);
         LoggerUtils.info("Entered mobile number: " + mobileNum);
 
-        // Wait and click credit fetch checkbox
         waitUntilClickable(driver.findElement(creditFetchCheckBox));
         driver.findElement(creditFetchCheckBox).click();
         LoggerUtils.info("Clicked on credit fetch checkbox");
 
-        // Wait and click proceed button
         waitUntilClickable(driver.findElement(proceedBtn));
         driver.findElement(proceedBtn).click();
         LoggerUtils.info("Clicked on proceed button");
     }
-
-    /**
-     * Optional: You can add more login-related flows here
-     */
 }
