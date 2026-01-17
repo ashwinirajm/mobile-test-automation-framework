fa34dd8653df780b2b000392806c0ac599c71575
Ashwini Raj <37695456+ashwinirajm@users.noreply.github.com>
2026-01-18 03:06:37 +0530

    Update HomePage.java

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -2,24 +2,34 @@ package pages;
 
 import base.BasePage;
 import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.MobileBy;
-import org.openqa.selenium.By;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import io.appium.java_client.pagefactory.iOSXCUITFindBy;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
 
 public class HomePage extends BasePage {
 
-    @AndroidFindBy(uiAutomator = "new UiSelector().className(\"android.widget.TextView\").textContains(\"garage\")")
-    @iOSXCUITFindBy(xpath = "//XCUIElementTypeStaticText[@name='garage']")
+    @AndroidFindBy(
+        uiAutomator = "new UiSelector().className(\"android.widget.TextView\").textContains(\"garage\")"
+    )
+    @iOSXCUITFindBy(
+        xpath = "//XCUIElementTypeStaticText[@name='garage']"
+    )
     private WebElement garageCTA;
 
     public HomePage(AppiumDriver driver) {
         super(driver);
+        PageFactory.initElements(
+            new AppiumFieldDecorator(driver),
+            this
+        );
     }
 
     public GarageHomePage navigateToGarageHome() {
-    if (isElementVisible(garageCTA)) {
-        garageCTA.click();
+        if (isElementVisible(garageCTA)) {
+            garageCTA.click();
+        }
+        return new GarageHomePage(driver);
     }
-    return new GarageHomePage(driver);
-  }  
 }
-
