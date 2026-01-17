5122ba6d1ee98f0fb7d4cc85e3d5b7ef40f406da
Ashwini Raj <37695456+ashwinirajm@users.noreply.github.com>
2026-01-18 02:33:16 +0530

    Update LoginDetailsTemplate.java

--- src/main/java/dataTemplates/LoginDetailsTemplate.java
+++ src/main/java/dataTemplates/LoginDetailsTemplate.java
@@ -1,29 +1,29 @@
-package dataTemplate.login;
+package dataTemplates;
 
 import br.com.six2six.fixturefactory.Fixture;
 import br.com.six2six.fixturefactory.Rule;
 import br.com.six2six.fixturefactory.loader.TemplateLoader;
-import dataTemplatePojo.login.LoginData;
+import dataTemplatesPojo.LoginDetails;
 
 /**
- * LoginTemplate
+ * LoginDetailsTemplate
  *
  * Purpose:
  * - Defines reusable login data variations
- * - Keeps test data separate from tests & pages
+ * - Keeps test data out of tests and pages
  */
-public class LoginTemplate implements TemplateLoader {
+public class LoginDetailsTemplate implements TemplateLoader {
 
     @Override
     public void load() {
 
-        Fixture.of(LoginData.class)
-                .addTemplate("validMobileLogin", new Rule() {{
+        Fixture.of(LoginDetails.class)
+                .addTemplate("validLogin", new Rule() {{
                     add("mobileNumber", "9876543210");
                 }});
 
-        Fixture.of(LoginData.class)
-                .addTemplate("invalidMobileLogin", new Rule() {{
+        Fixture.of(LoginDetails.class)
+                .addTemplate("invalidLogin", new Rule() {{
                     add("mobileNumber", "12345");
                 }});
     }
