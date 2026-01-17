3133ef60fc5c012d73eb98133109fa67cc6259bd
Ashwini Raj <37695456+ashwinirajm@users.noreply.github.com>
2026-01-18 03:04:21 +0530

    Update HomePage.java

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,1 +1,25 @@
+package pages;
+
+import base.BasePage;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
+import org.openqa.selenium.By;
+
+public class HomePage extends BasePage {
+
+    @AndroidFindBy(uiAutomator = "new UiSelector().className(\"android.widget.TextView\").textContains(\"garage\")")
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeStaticText[@name='garage']")
+    private WebElement garageCTA;
+
+    public HomePage(AppiumDriver driver) {
+        super(driver);
+    }
+
+    public GarageHomePage navigateToGarageHome() {
+    if (isElementVisible(garageCTA)) {
+        garageCTA.click();
+    }
+    return new GarageHomePage(driver);
+  }  
+}
 
