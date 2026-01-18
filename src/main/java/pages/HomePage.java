27a4ad0ef0e799b2f56bbf8f9d9c90b603e39f3a
Ashwini Raj <37695456+ashwinirajm@users.noreply.github.com>
2026-01-18 18:36:47 +0530

    Update HomePage.java

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -3,42 +3,25 @@ package pages;
 import base.BasePage;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.pagefactory.AndroidFindBy;
-import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 import io.appium.java_client.pagefactory.iOSXCUITFindBy;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.PageFactory;
 import utils.LoggerUtils;
 
 public class HomePage extends BasePage {
 
-    @AndroidFindBy(
-        uiAutomator = "new UiSelector().className(\"android.widget.TextView\").textContains(\"garage\")"
-    )
-    @iOSXCUITFindBy(
-        xpath = "//XCUIElementTypeStaticText[@name='garage']"
-    )
+    @AndroidFindBy(uiAutomator = "new UiSelector().className(\"android.widget.TextView\").textContains(\"garage\")")
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeStaticText[@name='garage']")
     private WebElement garageCTA;
 
     public HomePage(AppiumDriver driver) {
         super(driver);
-        PageFactory.initElements(
-            new AppiumFieldDecorator(driver),
-            this
-        );
     }
 
-    /**
-     * Navigate to Garage Home page.
-     * Automatically logs action and attaches screenshot.
-     */
     public GarageHomePage navigateToGarageHome() {
         if (isElementVisible(garageCTA)) {
             garageCTA.click();
-            LoggerUtils.info("Clicked on Garage CTA from Home Page");
-            LoggerUtils.attachScreenshot(driver(), "Clicked_GarageCTA");
-        } else {
-            LoggerUtils.warn("Garage CTA not visible on Home Page");
-            LoggerUtils.attachScreenshot(driver(), "GarageCTA_NotVisible");
+            LoggerUtils.info("Clicked on Garage CTA");
+            LoggerUtils.attachScreenshot(driver, "Home_ClickGarageCTA");
         }
         return new GarageHomePage(driver);
     }
